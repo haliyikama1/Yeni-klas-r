15e50d1ebad3cf488a9c02fd6a36957e4daa2d20
tahirbera <tahirberakeskin@gmail.com>
2024-11-23 13:04:58 +0300

    fw123

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
               </button>
               
               <button className={styles.buttonSecondary}>
-              <Link href="https://wa.me/905375198067?text=Merhabalar,%20hafriyat%20hizmetiyle%ilgili%bilgi%almak%için%yazıyorum">WhatsApp&apos;tan İletişime Geç</Link>
+              <Link href="https://wa.me/905375198067?text=Merhabalar,%20hafriyat%20hizmetiyle%20ilgili%20bilgi%20almak%20için%20yazıyorum">WhatsApp&apos;tan İletişime Geç</Link>
               </button>
             </div>
             
